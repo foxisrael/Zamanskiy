7f98b96005840b19a41b63d6f731f5a83760eb19
Dmitriy Z <dmitriy.zamanskiy@gmail.com>
2017-01-05 12:22:10 +0200

    mail

---
+++ mail.php
@@ -0,0 +1,13 @@
+<?php 
+    header('Content-Type:text/html; charset="UTF-8"');
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $text = $_POST['text'];
+    $to = 'mail@zamanskiy.ru';
+    $subject = 'Заполнена контактная форма с '.$_SERVER['HTTP_REFERER'];
+    $message = "
+    Пользователь ввел следующие данные:
+    Имя: $name
+    Email: $email
+    Текст сообщения: $text";
+    mail($to, $subject, $message);
